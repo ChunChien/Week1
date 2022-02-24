4e6a9bf85929980a7832ad9db51024d6d3f7f8ff
ChunChien <100334369+ChunChien@users.noreply.github.com>
2022-02-24 13:18:37 +0800

    Week1

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head></head>
+
+<body></body>
+<?php
+
+//變數與指定敘述
+$stu_name = "曾淳謙";
+$number = "A1074350";
+
+//顯示變數內容
+print "<b>姓名:</b>" . $stu_name."<br/>";
+print "<b>學號:</b>" . $number. "<br/>";
+
+?>
+
+<html></html>
