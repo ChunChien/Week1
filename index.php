9d5a63c319ab932e3b25753d75ca03af35834014
ChunChien <100334369+ChunChien@users.noreply.github.com>
2022-02-24 13:48:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head></head>
+
+<body></body>
+<?php
+
+$stu_name = '曾淳謙';
+$num = 'A1074350';
+
+echo "<b>Hello</b> PHP!</br>";
+
+print "姓名:".$stu_name."</br>";
+print "學號:".$num;
+
+
+
+?>
+
+<html></html>
